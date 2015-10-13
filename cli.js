7026042bccdb1e320a11036eb0ce0a58297bbc31
mguida22 <michael.guida@colorado.edu>
2015-10-13 13:31:11 -0600

    Add --all option Show all data for location with -a or --all flag

--- cli.js
+++ cli.js
@@ -4,10 +4,16 @@
 require('dotenv').load();
 const meow = require('meow');
 const request = require('request');
+const d2d = require('degrees-to-direction');
 
 let cli = meow(`
   Usage
     $ weather <city name>
+
+  Options
+    --version  Current version
+    --help     Show this message
+    -a, --all  Show all data
 
 	Example
 	  $ weather Boulder,CO
@@ -16,7 +22,11 @@ let cli = meow(`
     scattered clouds
     69.55 ° F
     15 % humidity
-`);
+`, {
+  alias: {
+    a: 'all'
+  }
+});
 
 if (cli.input[0] === undefined) {
   console.log(
@@ -29,7 +39,7 @@ if (cli.input[0] === undefined) {
 }
 
 let options = {
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + cli.input[0],
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + cli.input[0] + '&units=imperial',
   headers: {
     'x-api-key': process.env.API_KEY
   }
@@ -40,7 +50,7 @@ request(options, function (error, response, body) {
     body = JSON.parse(body);
 
     let description = body.weather[0].description;
-    let temp = (body.main.temp * (9/5) - 459.67).toFixed(2);
+    let temp = body.main.temp;
     let humidity = body.main.humidity;
 
     console.log(
@@ -49,6 +59,16 @@ request(options, function (error, response, body) {
       `  ${temp} ° F\n`,
       `  ${humidity} % humidity`
     );
+
+    if (cli.flags.all === true) {
+      //console.log(JSON.stringify(body));
+      console.log(
+        `\n`,
+        `  min. temp: ${body.main.temp_min}\n`,
+        `  max. temp: ${body.main.temp_max}\n`,
+        `  wind speed: ${body.wind.speed} mph from ${d2d(body.wind.deg)}`
+      );
+    }
   } else if (response.statusCode == 401){
     console.log('Error: No API key provided.');
   } else {
